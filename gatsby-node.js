5800f81aefdbaa987b8e1ba334643d0f5cb6677b
kjof <andreeastroem@gmail.com>
2021-07-09 01:08:34 +0200

    fix: start on product page

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,29 +1,31 @@
-const path = require('path')
+const path = require("path");
 
-exports.createPages = async ({graphql, actions}) => {
-    const { createPage } = actions;
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions;
 
-    const productpageTemplate = path.resolve(`src/templates/product-page.tsx`)
+  const productpageTemplate = path.resolve(`src/templates/product-page.tsx`);
 
-    const result = await graphql(`query MyQuery {
-        allContentfulProduct {
-          nodes {
-            id
-            name
-            slug
-          }
+  const result = await graphql(`
+    query MyQuery {
+      allContentfulProduct {
+        nodes {
+          id
+          name
+          slug
         }
-      }`)
-        console.log('result', result);
+      }
+    }
+  `);
+  console.log("result", result);
 
-        result?.data?.allContentfulProduct?.nodes.forEach((product) => {
-            console.log('slug', product.slug);
-            createPage({
-                path: `product/${product.slug}`,
-                component: productpageTemplate,
-                context: {
-
-                }
-            })
-        })
-}
+  result?.data?.allContentfulProduct?.nodes.forEach((product) => {
+    console.log("slug", product.slug);
+    createPage({
+      path: `product/${product.slug}`,
+      component: productpageTemplate,
+      context: {
+        id: product.id,
+      },
+    });
+  });
+};
--- src/templates/product-page.tsx
+++ src/templates/product-page.tsx
@@ -1,7 +1,39 @@
-import React from 'react';
-import {PageProps} from 'gatsby';
+import React from "react";
+import { PageProps, graphql } from "gatsby";
 
-export default function ProductPage(props: PageProps) {
-    return <div>Sicc produktsida</div>
+type Data = {
+  contentfulProduct: {
+    name: string;
+    tags?: Array<string>;
+    ingredients?: Array<string>;
+  };
+};
+type Context = {
+  id: string;
+};
+
+export default function ProductPage(
+  props: PageProps<Data, Context>
+): JSX.Element {
+  const { data } = props;
+
+  console.log("data", data);
+
+  return (
+    <div>
+      <h1>{data.contentfulProduct.name}</h1>
+      <p>Sicc produktsida</p>
+    </div>
+  );
 }
 
+export const pageQuery = graphql`
+  query MyQuery($id: String) {
+    contentfulProduct(id: { eq: $id }) {
+      id
+      name
+      tags
+      ingredients
+    }
+  }
+`;
