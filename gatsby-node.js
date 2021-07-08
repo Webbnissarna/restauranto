45cfed5aad2d97d7ab95b49991d5ac25f9bc308c
kjof <andreeastroem@gmail.com>
2021-07-09 00:17:01 +0200

    feat: generate product pages
    generate product pages based on the available product pages in the Contentful space

---
+++ gatsby-node.js
@@ -0,0 +1,29 @@
+const path = require('path')
+
+exports.createPages = async ({graphql, actions}) => {
+    const { createPage } = actions;
+
+    const productpageTemplate = path.resolve(`src/templates/product-page.tsx`)
+
+    const result = await graphql(`query MyQuery {
+        allContentfulProduct {
+          nodes {
+            id
+            name
+            slug
+          }
+        }
+      }`)
+        console.log('result', result);
+
+        result?.data?.allContentfulProduct?.nodes.forEach((product) => {
+            console.log('slug', product.slug);
+            createPage({
+                path: `product/${product.slug}`,
+                component: productpageTemplate,
+                context: {
+
+                }
+            })
+        })
+}
---
+++ src/templates/product-page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import {PageProps} from 'gatsby';
+
+export default function ProductPage(props: PageProps) {
+    return <div>Sicc produktsida</div>
+}
+
