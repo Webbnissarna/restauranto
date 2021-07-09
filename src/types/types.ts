589d3cd56a02d1a3469ef60e4bb774195ec48939
kjof <andreeastroem@gmail.com>
2021-07-09 18:38:12 +0200

    feat: extract types

---
+++ src/types/types.ts
@@ -0,0 +1,52 @@
+import { IGatsbyImageData } from "gatsby-plugin-image";
+
+export interface Image {
+  title?: string;
+  gatsbyImageData?: IGatsbyImageData;
+}
+
+export interface Tag {
+  color: string;
+  name: string;
+  image?: Image;
+}
+
+export interface ContentfulProduct {
+  name: string;
+  tags?: Array<Tag>;
+  ingredients?: Array<string>;
+  image?: Image;
+  price: number;
+  description?: {
+    description: string;
+  };
+}
+
+export interface ProductOverview {
+  name: string;
+  price: number;
+  discount: number;
+  image: Image;
+}
+
+export interface Contact {
+  adress: string;
+  email: string;
+  name: string;
+  phone: string;
+  location: {
+    lat: number;
+    lon: number;
+  };
+}
+
+export interface Homepage {
+  contact: Contact;
+  highlightedProducts: Array<ProductOverview>;
+  banner: ProductOverview;
+  categories: Array<Tag>;
+}
+
+export interface Context {
+  id: string;
+}
