6a8477e6a9a57930a0c15c1ad67742a6ddab840f
kjof <andreeastroem@gmail.com>
2021-07-09 15:36:35 +0200

    feat: map data from graphQL to product-page template

--- src/templates/product-page.tsx
+++ src/templates/product-page.tsx
@@ -1,11 +1,22 @@
 import React from "react";
 import { PageProps, graphql } from "gatsby";
+import { IGatsbyImageData, GatsbyImage } from "gatsby-plugin-image";
+
+type Image = {
+  title?: string;
+  gatsbyImageData?: IGatsbyImageData;
+};
 
 type Data = {
   contentfulProduct: {
     name: string;
     tags?: Array<string>;
     ingredients?: Array<string>;
+    image?: Image;
+    price: number;
+    description?: {
+      description: string;
+    };
   };
 };
 type Context = {
@@ -19,10 +30,31 @@ export default function ProductPage(
 
   console.log("data", data);
 
+  const ingredients = data?.contentfulProduct?.ingredients;
+  const tags = data?.contentfulProduct?.tags;
+  const price =
+    data?.contentfulProduct?.price && data?.contentfulProduct?.price / 100.0;
   return (
     <div>
       <h1>{data.contentfulProduct.name}</h1>
-      <p>Sicc produktsida</p>
+      <GatsbyImage
+        image={data.contentfulProduct.image.gatsbyImageData}
+        alt={data.contentfulProduct.image.title}
+      />
+      <ul>
+        {tags &&
+          tags.map((tag) => {
+            return <li>{tag}</li>;
+          })}
+      </ul>
+      <ul>
+        {ingredients &&
+          ingredients.map((ingredient) => {
+            return <li>{ingredient}</li>;
+          })}
+      </ul>
+      {price && <div>Price: {price} $</div>}
+      <section>{data?.contentfulProduct?.description?.description}</section>
     </div>
   );
 }
@@ -34,6 +66,27 @@ export const pageQuery = graphql`
       name
       tags
       ingredients
+      price
+      description {
+        description
+      }
+      image {
+        title
+        gatsbyImageData(width: 400)
+      }
+      nutrients {
+        calories
+        carbohydrates
+      }
+    }
+    contentfulContact {
+      location {
+        lat
+        lon
+      }
+      email
+      name
+      phone
     }
   }
 `;
