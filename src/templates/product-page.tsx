3b4d6e399e79a8e740642d8b72902eb5543ecb81
Master Kenth <masterkenth@gmail.com>
2021-07-10 17:07:36 +0200

    feat: implement product page design

--- src/templates/product-page.tsx
+++ src/templates/product-page.tsx
@@ -1,10 +1,14 @@
-import React from "react";
+/** @jsx jsx */
+import { jsx } from "theme-ui";
+
 import { PageProps, graphql } from "gatsby";
 import { GatsbyImage } from "gatsby-plugin-image";
-import { ContentfulProduct, Context } from "../types/types";
+import { Contact, ContentfulProduct, Context } from "../types/types";
+import RootContainer from "../components/atoms/RootContainer";
 
 interface Data {
   contentfulProduct: ContentfulProduct;
+  contentfulContact: Contact;
 }
 
 export default function ProductPage(
@@ -14,30 +18,181 @@ export default function ProductPage(
 
   const ingredients = data?.contentfulProduct?.ingredients;
   const tags = data?.contentfulProduct?.tags;
-  const price =
-    data?.contentfulProduct?.price && data?.contentfulProduct?.price / 100.0;
+  const nutrients = data?.contentfulProduct?.nutrients;
+  const priceText = (
+    data?.contentfulProduct?.price && data?.contentfulProduct?.price / 100.0
+  )
+    .toFixed(2)
+    .replace(".", ",");
+
   return (
-    <div>
-      <h1>{data.contentfulProduct.name}</h1>
+    <RootContainer>
+      <div
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          textAlign: "center",
+          gap: "md",
+        }}
+      >
+        <h1>{data.contentfulContact.name}</h1>
+        <span>{data.contentfulContact.phone}</span>
+      </div>
+
+      <h2
+        sx={{
+          textAlign: "center",
+          margin: 0,
+        }}
+      >
+        {data.contentfulProduct.name}
+      </h2>
+
+      <span
+        sx={{
+          textAlign: "center",
+          fontSize: "h3",
+          fontStyle: "italic",
+        }}
+      >
+        {priceText}:-
+      </span>
+
       <GatsbyImage
         image={data.contentfulProduct.image.gatsbyImageData}
         alt={data.contentfulProduct.image.title}
+        sx={{
+          height: 216,
+        }}
       />
-      <ul>
-        {tags &&
-          tags.map((tag) => {
-            return <li>{tag.name}</li>;
-          })}
-      </ul>
-      <ul>
-        {ingredients &&
-          ingredients.map((ingredient) => {
-            return <li>{ingredient}</li>;
-          })}
-      </ul>
-      {price && <div>Price: {price} $</div>}
-      <section>{data?.contentfulProduct?.description?.description}</section>
-    </div>
+
+      <div>
+        <ul
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "center",
+            listStyle: "none",
+            gap: "xl",
+            margin: 0,
+            padding: 0,
+          }}
+        >
+          {tags &&
+            tags.map((tag) => {
+              return (
+                <li
+                  key={tag.name}
+                  sx={{
+                    backgroundColor: tag.color,
+                    fontSize: "small",
+                    margin: 0,
+                    paddingX: "sm",
+                    paddingY: "2xs",
+                    borderRadius: "2xs",
+                  }}
+                >
+                  {tag.name}
+                </li>
+              );
+            })}
+        </ul>
+      </div>
+
+      <section
+        title="Description"
+        sx={{
+          marginX: 60,
+        }}
+      >
+        <span>{data.contentfulProduct.description.description}</span>
+      </section>
+
+      <section
+        title="Ingredients"
+        sx={{
+          width: "100%",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          textAlign: "center",
+          gap: "md",
+        }}
+      >
+        <h3 sx={{ margin: 0, padding: 0 }}>Ingredients</h3>
+        <ul
+          sx={{
+            width: "100%",
+            listStyle: "none",
+            display: "flex",
+            flexDirection: "column",
+            margin: 0,
+            padding: 0,
+            justifyContent: "center",
+
+            li: {
+              borderTop: "transparentWhite",
+              paddingY: "xs",
+
+              "&:last-of-type": {
+                borderBottom: "transparentWhite",
+              },
+            },
+          }}
+        >
+          {ingredients &&
+            ingredients.map((ingredient) => {
+              return <li key={ingredient}>{ingredient}</li>;
+            })}
+        </ul>
+      </section>
+
+      <section
+        title="Nutrients"
+        sx={{
+          width: "100%",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          textAlign: "center",
+          gap: "md",
+        }}
+      >
+        <h3 sx={{ margin: 0, padding: 0 }}>Nutrients</h3>
+        <ul
+          sx={{
+            width: "100%",
+            listStyle: "none",
+            display: "flex",
+            flexDirection: "column",
+            margin: 0,
+            padding: 0,
+            justifyContent: "center",
+
+            li: {
+              borderTop: "transparentWhite",
+              paddingY: "xs",
+
+              "&:last-of-type": {
+                borderBottom: "transparentWhite",
+              },
+            },
+          }}
+        >
+          {nutrients &&
+            Object.keys(nutrients)
+              .filter((key) => nutrients[key])
+              .map((nutrientName) => {
+                const value = nutrients[nutrientName];
+                return (
+                  <li key={nutrientName}>
+                    {nutrientName}: {value}
+                  </li>
+                );
+              })}
+        </ul>
+      </section>
+    </RootContainer>
   );
 }
 
@@ -65,11 +220,6 @@ export const pageQuery = graphql`
       }
     }
     contentfulContact {
-      location {
-        lat
-        lon
-      }
-      email
       name
       phone
     }
