1c7266bd1e3803489bc2a813b27a8adb52282fd8
kjof <andreeastroem@gmail.com>
2021-07-10 11:59:14 +0200

    chore: update theme file with new values

--- src/gatsby-plugin-theme-ui/theme.ts
+++ src/gatsby-plugin-theme-ui/theme.ts
@@ -6,6 +6,23 @@ const theme = {
     base: "1rem",
     small: "0.75rem",
   },
+  space: {
+    "2xs": "0.125rem",
+    xs: "0.25rem",
+    sm: "0.5rem",
+    md: "0.625rem",
+    lg: "0.75rem",
+    xl: "1rem",
+    "2xl": "1.25rem",
+  },
+  radii: {
+    default: "0.625rem",
+  },
+  colors: {
+    base: "white",
+    phone: "#4FA0FF",
+    inverted: "black",
+  },
 };
 
 export default theme;
